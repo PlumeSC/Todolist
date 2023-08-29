64a0b48d5a44340d53c746016c4dcc8b66853e33
PlumeSC <supawit.chukiat@gmail.com>
2023-08-29 15:21:50 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './App.scss';
+
 function App() {
-    return <div className="App">
-      <div>React App?????</div>
-    </div>;
+    return (
+        <div className="todo">
+            <div className="todo__header">Header</div>
+            <div className="todo__sidebar">SideBar</div>
+            <div classNAme="todo_content">TodoContent</div>
+        </div>
+    );
 }
 
 export default App;
