47b01b441a1b8eb96faf9fe53adbe9adf80671cf
PlumeSC <supawit.chukiat@gmail.com>
2023-08-30 14:07:27 +0700

    accordion

--- src/app/App.js
+++ src/app/App.js
@@ -12,13 +12,27 @@ function App() {
                 <aside className='sidebar'>
                     <section className='sidebar_category'>
                         <ul className='list'>
-                            <Listitem fa={<FaInbox className='list_item_icon'/>} text='Inbox'/>
-                            <Listitem fa={<FaCalendar className='list_item_icon'/>} text='Today'/>
-                            <Listitem fa={<FaCalendarAlt className='list_item_icon'/>} text='Next 7 days'/>
+                            <Listitem fa={<FaInbox/>} text='Inbox'/>
+                            <Listitem fa={<FaCalendar/>} text='Today'/>
+                            <Listitem fa={<FaCalendarAlt/>} text='Next 7 days'/>
                         </ul>
                     </section>
                     <section className='sidebar_category'>
-
+                        <div className='accordion'>
+                            <div className='accordion_toggle'>
+                                <li className='accordion_item'>
+                                    <FaChevronDown className='accordion_icon accordion_icon_active'/>
+                                    <p className='accordion_text'>Project</p>
+                                </li>
+                                <ul className='list'>
+                                    <div></div>
+                                    <Listitem fa={<FaInbox/>} text='ProjectA'/>
+                                    <Listitem fa={<FaInbox/>} text='ProjectB'/>
+                                    <Listitem fa={<FaInbox/>} text='ProjectC'/>
+                                </ul>
+                            </div>
+                        </div>
+                        
                     </section>
                 </aside>
             </div>
--- src/component/Listitem.jsx
+++ src/component/Listitem.jsx
@@ -2,10 +2,11 @@ import './Listitem.scss'
 
 
 export function Listitem(x){
+    const listClassName = `list_item ${x.active ?`active`:``}`
     return(
-    <li className='list_item'>
-        <span >{x.fa}</span>
-        <p className='list_list_text'>{x.text}</p>
+    <li className={listClassName}>
+        <span>{x.fa}</span>
+        <p className='list_item_text'>{x.text}</p>
     </li>
     )
 }
